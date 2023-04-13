f1658af231351db02c5a2af2743cfb17d28d0e41
Abhi Pandey <97884726+abhipandey1402@users.noreply.github.com>
2023-04-13 16:48:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	var firstName = document.forms["form1"]["fname"].value;
+    var lastName = document.forms["form1"]["lname"].value;
 
+	alert(firstName + " " + lastName);
 }
